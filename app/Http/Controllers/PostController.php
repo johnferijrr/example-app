3790edff9065351ccbdf75698d72e2c4bdc76a97
johnferijrr <johnferijr@gmail.com>
2023-05-29 19:56:52 +0700

    new

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\PostDetailResource;
 
 class PostController extends Controller
 {
     public function index()
     {
         $posts = Post::all();
-        return response()->json($posts);
+        return PostResource::collection($posts); 
+    }
+
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+        return new PostDetailResource($post); 
     }
 }
