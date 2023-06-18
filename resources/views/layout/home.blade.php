f81e8b7ef3ce5e7b1b0f15c4dea7e8a732cf577b
johnferijrr <johnferijr@gmail.com>
2023-06-18 11:20:20 +0700

    new

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -196,33 +196,6 @@
     <img class="bw" src="images/l-1.png">
     <img class="bx" src="images/l-2.png">
     <img class="by" src="images/l-3.png">
-    <div class="bz">
-        <script src="http://maps.googleapis.com/maps/api/js"></script>
-        <script>
-        function initialize() {
-          var propertiPeta = {
-            center:new google.maps.LatLng(-8.5830695,116.3202515),
-            zoom:9,
-            mapTypeId:google.maps.MapTypeId.ROADMAP
-          };
-          
-          var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
-          
-          // membuat Marker
-          var marker=new google.maps.Marker({
-              position: new google.maps.LatLng(-7.774588123293439, 110.37450370080577),
-              map: peta
-          });
-        
-        }
-        
-        // event jendela di-load  
-        google.maps.event.addDomListener(window, 'load', initialize);
-        </script>
-
-          <div id="googleMap" style="width:100%;height:380px;"></div>
-          
-</div>
       <a href="#" class="ca">Lihat Selengkapnya...</a>
       <a href="#" class="cb">Lihat Selengkapnya...</a>
       <a href="#" class="cc">Lihat Selengkapnya...</a>
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -12,10 +12,39 @@
     <style>
         @import url("https://fonts.googleapis.com/css2?family=Inter&display=swap");
     </style>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+    crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
 </head>
 
 <body>
 
+    <div id="map" style="width: 906.75px; height: 338.25px; left: 267.75px; top: 3150px; position: absolute; border-radius: 43px;"></div>
+    <script>
+
+	const map = L.map('map').setView([-7.775440423721329, 110.37424686589556], 13);
+
+	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 50000,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+
+	const marker = L.marker([-7.775440423721329, 110.37424686589556]).addTo(map)
+		.bindPopup('Sekolah Vokasi UGM at here!').openPopup();
+
+	function onMapClick(e) {
+		popup
+			.setLatLng(e.latlng)
+			.setContent(`You clicked the map at ${e.latlng.toString()}`)
+			.openOn(map);
+	}
+
+	map.on('click', onMapClick);
+
+    </script>
 
 
     {{-- content --}}
